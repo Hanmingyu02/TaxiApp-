ea284a2dcd06c9d25d8a78da83d5969be8fc9b09
Hanmingyu02 <hmg0835@naver.com>
2025-03-16 18:01:57 +0900

    시작

---
+++ src/TaxiApp.tsx
@@ -0,0 +1,25 @@
+import React, { JSX } from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function TaxiApp(): JSX.Element {
+  console.log('--TaxiApp()');
+  return (
+    <SafeAreaView>
+      <Text style={style.textBlack}>Hello React Native</Text>
+      <Text style={style.textBlue}>Hello React Native</Text>
+    </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  textBlack: {
+    fontSize: 18,
+    color: 'black',
+  },
+  textBlue: {
+    fontSize: 18,
+    color: 'blue',
+  },
+});
+
+export default TaxiApp;
